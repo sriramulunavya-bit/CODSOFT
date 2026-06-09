931e20af4a6587bb62a8e7a3a5e7ddfb106e0b19
NAVYA SREE SRIRAMULU <sriramulunavya@gmail.com>
2026-06-09 16:29:57 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,53 @@
+const display = document.getElementById("display");
+
+const buttons = document.querySelectorAll("button");
+
+let expression = "";
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const value = button.innerText;
+
+        if(value === "C"){
+
+            expression = "";
+            display.value = "";
+
+        }
+
+        else if(value === "←"){
+
+            expression = expression.slice(0,-1);
+            display.value = expression;
+
+        }
+
+        else if(value === "="){
+
+            try{
+
+                expression = eval(expression).toString();
+                display.value = expression;
+
+            }
+            catch{
+
+                display.value = "Error";
+                expression = "";
+
+            }
+
+        }
+
+        else{
+
+            expression += value;
+            display.value = expression;
+
+        }
+
+    });
+
+});
